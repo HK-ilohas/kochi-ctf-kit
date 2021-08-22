d72e349956f27f7ef204c32b328cfd2e9ac88164
olta2000 <guway.olta@gmail.com>
2021-08-22 12:48:28 +0900

    initialize directory

---
+++ go.mod
@@ -0,0 +1,7 @@
+module ownserver
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo/v4 v4.3.0
+)
---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"ownserver/handler"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/login", handler.Login())
+
+	users := e.Group("/users")
+	users.Use(middleware.JWT([]byte("secret")))
+	users.GET("/welcome", handler.Restricted()) // users/welcome
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+	e.GET("/api", api.getTeamData())
+}
